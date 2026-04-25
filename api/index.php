a3a0f436ab42794ab0ae100cd4376a2aba295e9b
10969ipan <arfianirfan48@gmail.com>
2026-04-25 19:21:08 +0700

    fix: assets serving and https enforcement

--- api/index.php
+++ api/index.php
@@ -8,4 +8,9 @@ putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('APP_CONFIG_CACHE=/tmp/config.php');
 putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 
+// Paksa HTTPS di Vercel
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 require __DIR__ . '/../public/index.php';
