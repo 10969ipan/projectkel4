90766120e3c1171e20f9ee01bf82fd4bb0b3e476
10969ipan <arfianirfan48@gmail.com>
2026-04-25 19:13:39 +0700

    fix: override bootstrap cache and storage to /tmp for vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 
 // Bridge Laravel ke Vercel
+// Pindahkan storage dan cache ke /tmp agar bisa ditulis (Writable)
+putenv('APP_STORAGE=/tmp');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+
 require __DIR__ . '/../public/index.php';
